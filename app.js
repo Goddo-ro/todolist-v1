66e8173db767b149898bda0c3569bc0b9c4eedcb
Goddo-ro <r0manovic1@yandex.ru>
2022-02-04 19:14:08 +0300

    Add ejs

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", function(req, res) {
-  res.send("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+
+  if (currentDay === 6 || currentDay == 0) {
+    day = "Weekend";
+  } else {
+    day = "Weekday";
+  }
+  res.render("list", {day: day});
 });
 
 app.listen(3000, function() {
