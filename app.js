5017b14c529b9bd45825168cb50e7f49ee2697b0
Goddo-ro <r0manovic1@yandex.ru>
2022-02-04 19:56:30 +0300

    Add post form and function on the server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
@@ -14,6 +15,11 @@ app.get("/", function(req, res) {
   }
   var day = today.toLocaleDateString("en-US", options);
   res.render("list", {day: day});
+});
+
+app.post("/", function(req, res) {
+  var item = req.body.toDoItem;
+  console.log(item);
 });
 
 app.listen(3000, function() {
